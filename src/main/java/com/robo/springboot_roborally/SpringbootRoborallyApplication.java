aa36d6ac6049e297460380115af1a8d03a95b000
Daniall <S235070@student.dtu.dk>
2024-06-11 15:48:58 +0200

    Setup downgrade to 17

---
+++ src/main/java/com/robo/springboot_roborally/SpringbootRoborallyApplication.java
@@ -0,0 +1,13 @@
+package com.robo.springboot_roborally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRoborallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRoborallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robo/springboot_roborally/SpringbootRoborallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robo.springboot_roborally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRoborallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
